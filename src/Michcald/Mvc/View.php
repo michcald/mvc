96a0837bf1af06f58a7c0548220642a64781b3e8
Michael <michcald@gmail.com>
2014-03-08 20:02:59 +0000

    Adding the view class

--- src/Michcald/Mvc/View.php
+++ src/Michcald/Mvc/View.php
@@ -4,5 +4,19 @@ namespace Michcald\Mvc;
 
 class View
 {
-    
+    public function render($file, array $data = array())
+    {
+        if(!file_exists($file)) {
+            throw new \Exception('View file not found: ' . $file);
+        }
+
+        $_ = $data;
+
+        ob_start();
+        include $file;
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
 }
