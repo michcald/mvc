0ec33c996cd17dacc93b80a30ed95ded7a58b04e
Michael <michcald@gmail.com>
2014-03-25 23:27:29 +0000

    CliController write and writeln with text color tags

--- src/Michcald/Mvc/Controller/CliController.php
+++ src/Michcald/Mvc/Controller/CliController.php
@@ -4,30 +4,61 @@ namespace Michcald\Mvc\Controller;
 
 abstract class CliController extends \Michcald\Mvc\Controller
 {
+    private $textColors = array(
+        'black'        => '\033[0;30m',
+        'red'          => '\033[0;31m',
+        'green'        => '\033[0;32m',
+        'brown'        => '\033[0;33m',
+        'blue'         => '\033[0;34m',
+        'purple'       => '\033[0;35m',
+        'cyan'         => '\033[0;36m',
+        'light-gray'   => '\033[0;37m',
+        'darl-gray'    => '\033[1;30m',
+        'light-red'    => '\033[1;31m',
+        'light-green'  => '\033[1;32m',
+        'yellow'       => '\033[1;33m',
+        'light-blue'   => '\033[1;34m',
+        'light-purple' => '\033[1;35m',
+        'light-cyan'   => '\033[1;36m',
+        'white'        => '\033[1;37m',
+    );
+    
+    private $bgColors = array(
+        'black'      => '\033[0;40m',
+        'red'        => '\033[0;41m',
+        'green'      => '\033[0;42m',
+        'brown'      => '\033[0;43m',
+        'blue'       => '\033[0;44m',
+        'purple'     => '\033[0;45m',
+        'cyan'       => '\033[0;46m',
+        'light-gray' => '\033[0;47m'
+    );
+    
+    private function convertToRealColor($string)
+    {
+        foreach ($this->textColors as $color => $code) {
+            $string = str_replace('<' . $color . '>', $code, $string);
+            $string = str_replace('</' . $color . '>', "\033[0m", $string);
+        }
+        
+        foreach ($this->bgColors as $color => $code) {
+            $string = str_replace('<bg-' . $color . '>', $code, $string);
+            $string = str_replace('</bg-' . $color . '>', "\033[0m", $string);
+        }
+        
+        return $string;
+    }
 
     final public function write($string)
     {
-        echo $string . "\n";
+        echo $this->convertToRealColor($string);
     }
-
-    final public function writeError($string)
+    
+    final public function writeln($string)
     {
-        echo "\033" . "[41m" . $string . "\033" . "[0m" . "\n";
-    }
-
-    final public function writeSuccess($string)
-    {
-        echo "\033" . "[42m" . $string . "\033" . "[0m" . "\n";
-    }
-
-    final public function writeWarning($string)
-    {
-        echo "\033" . "[43m" . $string . "\033" . "[0m" . "\n";
-    }
-
-    final public function writeInfo($string)
-    {
-        echo "\033" . "[44m" . $string . "\033" . "[0m" . "\n";
+        $this->write($string);
+        
+        echo "\n";
     }
 
     final public function confirm()
