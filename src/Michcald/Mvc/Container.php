ade97bd2140cd30866438d65e0e3e2dc564a271e
Michael Caldera <michcald@gmail.com>
2014-09-12 13:58:12 +0200

    Adding default value on get container

--- src/Michcald/Mvc/Container.php
+++ src/Michcald/Mvc/Container.php
@@ -11,9 +11,12 @@ abstract class Container
         self::$services[$id] = $service;
     }
 
-    static public function get($id)
+    static public function get($id, $default = null)
     {
         if (!array_key_exists($id, self::$services)) {
+            if ($default != null) {
+                return $default;
+            }
             throw new \Exception('Service not found: ' . $id);
         }
 
